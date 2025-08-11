6081b2315809af0e8558c5c3c3cf37f4f0e015b7
dhirajyadav91 <dhirajdhoni97@gmail.com>
2025-08-11 03:41:48 -0700

    v3

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -5,7 +5,7 @@ const AboutMeText = () => {
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p>
-        I’m Dhiraj, a web developer
+        I’m Dhiraj, A Web Developer
       </p>
       <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
         <Link
--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
     <div className="relative self-end h-full w-full items-center justify-center">
       <div className=" h-full w-full">
         <img
-          src="../../public/images/dhiraj1.png"
+          src="/images/dhirajpic6.png"
           alt="Dhiraj Yadav"
           className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
         />
